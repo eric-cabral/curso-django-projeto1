f3a736674c0b5aa009ac2c95f9b9b07c1826882d
Eric Cabral <eric2509.cabral@gmail.com>
2023-07-24 00:21:45 -0300

    nova url e view

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Eric Cabral',
     })
+
+
+def recipes(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Eric Cabral',
+    })
