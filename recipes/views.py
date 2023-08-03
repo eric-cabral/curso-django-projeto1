b615c8a3637828c985b905160f28de7527fd9f32
Eric Cabral <eric2509.cabral@gmail.com>
2023-08-02 22:55:23 -0300

    ocultando receitas nao publicadas

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,9 @@ from .models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
@@ -19,8 +21,9 @@ def recipes(request, id):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(
-        category__id=category_id
+        category__id=category_id,
+        is_published=True,
         ).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
     })
