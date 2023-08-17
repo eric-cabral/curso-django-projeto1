3bdc99ea47b166c16ad83333794cae051b170237
Eric Cabral <eric2509.cabral@gmail.com>
2023-08-17 13:08:26 -0300

    criando classe base para testes

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,10 +1,12 @@
-from django.test import TestCase
 from django.urls import reverse, resolve
 from recipes import views
-from recipes.models import Category, Recipe, User
+from .test_recipe_base import RecipeTestBase
 
 
-class RecipeViewsTest(TestCase):
+class RecipeViewsTest(RecipeTestBase):
+    def tearDown(self) -> None:
+        return super().tearDown()
+
     def test_recipe_home_views_fuction_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
@@ -25,28 +27,6 @@ class RecipeViewsTest(TestCase):
         )
 
     def test_recipe_home_template_loads_recipes(self):
-        category = Category.objects.create(name='Category')
-        author = User.objects.create_user(
-            first_name='user',
-            last_name='name',
-            username='username',
-            password='123456',
-            email='username@email.com',
-        )
-        recipe = Recipe.objects.create(
-            category=category,
-            author=author,
-            title='Recipe Title',
-            description='Recipe Description',
-            slug='recipe=slug',
-            preparation_time=10,
-            preparation_time_unit='Minutos',
-            servings=5,
-            servings_unit='Porções',
-            preparation_steps='Recipe Preparation Steps',
-            preparation_steps_is_html=False,
-            is_published=True,
-        )
         response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
         response_context_recipes = response.context['recipes']
@@ -65,7 +45,7 @@ class RecipeViewsTest(TestCase):
         self.assertEqual(response.status_code, 404)
 
     # def test_recipe_category_template_shoes_no_recipes_found_if_no_recipes(self):  # noqa: E501
-    #     response = self.client.get(reverse('recipes:home', kwargs={'category_id': 1}))  # noqa: E501
+    #     response = self.client.get(reverse('recipes:category', kwargs={'category_id': 1}))  # noqa: E501
     #     self.assertIn(
     #         'No found recipes here',
     #         response.content.decode('utf-8')
